ef633b0c328f04a1fa3c12a9bc95af7805102cfc
Pachicobue <tigerssho@gmail.com>
2023-04-25 21:00:56 +0900

    ダブリングのリファクタリング

--- src/algorithm/doubling.hpp
+++ src/algorithm/doubling.hpp
@@ -3,12 +3,12 @@
 class Doubling
 {
 public:
-    Doubling(const Vec<int>& nexts, i64 K)
+    Doubling(const Vec<int>& f, i64 K)
     {
-        const int N = nexts.size();
+        const int N = f.size();
         const int lg = bitWidth(K);
         m_nextss.assign(lg, Vec<int>(N, -1));
-        m_nextss[0] = nexts;
+        m_nextss[0] = f;
         for (int d : rep(lg - 1)) {
             for (int i : rep(N)) {
                 if (m_nextss[d][i] == -1) { continue; }
@@ -16,35 +16,34 @@ public:
             }
         }
     }
-    Doubling(const Vec<int>& nexts) : Doubling(nexts, (i64)nexts.size()) {}
 
-    int kthNext(int p, i64 k) const
+    int kthNext(int x, i64 k) const
     {
         const int N = m_nextss[0].size();
         const int lg = m_nextss.size();
         assert(k < (1_i64 << lg));
         for (int d : rep(lg)) {
-            if (0 <= p and p < N) {
-                if (isBitOn(k, d)) { p = m_nextss[d][p]; }
+            if (0 <= x and x < N) {
+                if (isBitOn(k, d)) { x = m_nextss[d][x]; }
             }
         }
-        return p;
+        return x;
     }
 
     template<typename F>
-    i64 distance(int p, F f) const
+    i64 distance(int x, F pred) const
     {
-        if (f(p)) { return 0; }
+        if (pred(x)) { return 0; }
         const int lg = m_nextss.size();
         i64 ans = 0;
         for (int d : per(lg)) {
-            const int np = m_nextss[d][p];
-            if (not f(np)) {
-                p = np;
+            const int np = m_nextss[d][x];
+            if (not pred(np)) {
+                x = np;
                 ans += (1_i64 << d);
             }
         }
-        if (f(m_nextss[0][p])) {
+        if (pred(m_nextss[0][x])) {
             return ans + 1;
         } else {
             return -1;
