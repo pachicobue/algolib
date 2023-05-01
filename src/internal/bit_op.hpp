6cf09164c7c0e780f03e01381f10c81ce68234b4
Pachicobue <tigerssho@gmail.com>
2023-05-02 01:40:43 +0900

    リファクタリング

--- src/internal/bit_op.hpp
+++ src/internal/bit_op.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#include "bit_op/btest.hpp"
-#include "bit_op/ceilfloor.hpp"
-#include "bit_op/mask.hpp"
-#include "bit_op/msblsb.hpp"
-#include "bit_op/popcount.hpp"
+#include "bit_ops/btest.hpp"
+#include "bit_ops/ceilfloor.hpp"
+#include "bit_ops/mask.hpp"
+#include "bit_ops/msblsb.hpp"
+#include "bit_ops/popcount.hpp"
